a8adefcf26a7e2b69ef77829a5e32299466b7329
= <=>
2024-04-27 01:22:15 +0100

    gnome.nix

---
+++ home/packages/work/gnome.nix
@@ -0,0 +1,21 @@
+{ config, pkgs, lib, ... }:
+
+{
+  home.packages = with pkgs; [
+    gnome-extension-manager
+    gnomeExtensions.tophat # system resource monitor
+    gnomeExtensions.clipboard-indicator
+    gnomeExtensions.dash-to-dock
+  ];
+
+  dconf.settings = {
+    "org/gnome/shell" = {
+      disable-user-extensions = false;
+      enabled-extensions = [
+        "tophat@fflewddur.github.io"
+        "clipboard-indicator@tudmotu.com"
+        "dash-to-dock@micxgx.gmail.com"
+      ];
+    };
+  };
+}
