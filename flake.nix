8382be6ca6a10136218656d1b41aa433456109c8
archiejudd <archie.judd@yahoo.com>
2024-05-04 17:33:42 +0100

    kolide -> kolide-launcher

--- flake.nix
+++ flake.nix
@@ -5,20 +5,20 @@
     nixpkgs.url = "github:nixos/nixpkgs/nixos-unstable";
     home-manager.url = "github:nix-community/home-manager";
     home-manager.inputs.nixpkgs.follows = "nixpkgs";
-    kolide = {
+    kolide-launcher = {
       url = "github:/kolide/nix-agent/main";
       inputs.nixpkgs.follows = "nixpkgs";
     };
 
   };
 
-  outputs = inputs@{ nixpkgs, home-manager, kolide, ... }: {
+  outputs = inputs@{ nixpkgs, home-manager, kolide-launcher, ... }: {
     nixosConfigurations = {
       xps-9510 = nixpkgs.lib.nixosSystem {
         system = "x86_64-linux";
         modules = [
           ./system/xps-9510/configuration.nix
-          kolide.nixosModules.kolide-launcher
+          kolide-launcher.nixosModules.kolide-launcher
           home-manager.nixosModules.home-manager
           {
             home-manager.useGlobalPkgs = true;
