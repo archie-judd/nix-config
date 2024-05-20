f18082dfa98f27651502d489e492fd866bedd5b1
archiejudd <archie.judd@yahoo.com>
2024-05-20 18:09:47 +0100

    fix option change

--- system/hosts/xps-9510/configuration.nix
+++ system/hosts/xps-9510/configuration.nix
@@ -90,8 +90,8 @@
   };
 
   # Enable touchpad support (enabled default in most desktopManager).
-  services.xserver.libinput.enable = true;
-  services.xserver.libinput.touchpad.tapping = true;
+  services.libinput.enable = true;
+  services.libinput.touchpad.tapping = true;
 
   # Define a user account. Don't forget to set a password with ‘passwd’.
   users.users.archie = {
