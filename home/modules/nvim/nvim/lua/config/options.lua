c4372e11bc122d6e01c886f15e4d5d340acf3146
archiejudd <archie.judd@yahoo.com>
2024-05-23 22:57:10 +0100

    global statusbar

--- home/modules/nvim/nvim/lua/config/options.lua
+++ home/modules/nvim/nvim/lua/config/options.lua
@@ -5,5 +5,6 @@ vim.o.clipboard = "unnamedplus"
 vim.o.pumheight = 10
 vim.o.completeopt = "menuone,noinsert,preview"
 vim.o.shortmess = "c" -- 'c' means 'don't show ins-completion mode messages'
+vim.o.laststatus = 3 -- only one statusline
 vim.cmd.colorscheme("catppuccin-mocha")
 vim.api.nvim_set_hl(0, "NormalFloat", { fg = "none", bg = "none" }) -- fix oil background
