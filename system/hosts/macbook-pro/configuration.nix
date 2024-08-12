fc644f9083da4f133344aaaae3f96d669f0eb62d
archiejudd <archie.judd@yahoo.com>
2024-08-12 18:10:16 +0100

    various macos changes

--- system/hosts/macbook-pro/configuration.nix
+++ system/hosts/macbook-pro/configuration.nix
@@ -44,8 +44,23 @@
   # default to list view in finder
   system.defaults.finder.FXPreferredViewStyle = "Nlsv";
 
+  # hotcorners
+  system.defaults.dock.wvous-tl-corner = 2; # top left - mission-control
+  system.defaults.dock.wvous-tr-corner = 4; # top right - desktop
+  system.defaults.dock.wvous-br-corner = 1; # disabled
+  system.defaults.dock.wvous-bl-corner = 1; # disabled
+
+  # whether to allow quitting of finder
+  system.defaults.finder.QuitMenuItem = false;
+
+  # whether to show the finder path bar
+  system.defaults.finder.ShowPathbar = true;
+  # whether to show the finder status bar
+  system.defaults.finder.ShowStatusBar = false;
+
   # trackpad speed (1.0 - 3.0)
   system.defaults.NSGlobalDomain."com.apple.trackpad.scaling" = 3.0;
 
+  # disable startup chime
   system.startup.chime = false;
 }
