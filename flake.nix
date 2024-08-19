5f5932cbefe0bca93108cf505f0136e625b150e7
archiejudd <archie.judd@yahoo.com>
2024-08-19 13:04:15 +0100

    bbc-to-spotify update

--- flake.nix
+++ flake.nix
@@ -13,7 +13,7 @@
     nix-darwin.inputs.nixpkgs.follows = "nixpkgs";
 
     bbc-to-spotify.url =
-      "github:archie-judd/bbc-to-spotify?ref=refs/tags/v0.0.5";
+      "github:archie-judd/bbc-to-spotify?ref=refs/tags/v0.0.1";
     bbc-to-spotify.inputs.nixpkgs.follows = "nixpkgs";
 
     kolide-launcher = {
