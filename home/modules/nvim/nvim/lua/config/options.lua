47f1fd590fc958980a14544e0020ebfef6a57140
archiejudd <archie.judd@yahoo.com>
2024-08-24 18:41:22 +0100

    shortmess+=F

--- home/modules/nvim/nvim/lua/config/options.lua
+++ home/modules/nvim/nvim/lua/config/options.lua
@@ -3,7 +3,7 @@ vim.wo.relativenumber = true
 vim.o.splitright = true
 vim.o.pumheight = 10
 vim.o.completeopt = "menuone,noinsert,preview"
-vim.o.shortmess = "ct" -- 'c' means 'don't show ins-completion mode messages', 't' means truncate file message when opening a file
+vim.o.shortmess = "ctF" -- 'c' means 'don't show ins-completion mode messages', 't' means truncate file message when opening a file, 'F' means don't show file edit messages upon opening
 vim.o.laststatus = 3 -- only one statusline
 vim.cmd.colorscheme("catppuccin-mocha")
 vim.api.nvim_set_hl(0, "NormalFloat", { fg = "none", bg = "none" }) -- fix oil background
