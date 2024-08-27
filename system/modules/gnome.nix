6267ea6624c10661d9a3aa64a8d0e138cb384f43
archiejudd <archie.judd@yahoo.com>
2024-08-27 13:45:54 +0100

    remove .gnome. package namespace

--- system/modules/gnome.nix
+++ system/modules/gnome.nix
@@ -2,17 +2,13 @@
 
 {
   environment.gnome.excludePackages = [
-    pkgs.gnome.gnome-contacts
-    pkgs.gnome.gnome-clocks
-    pkgs.gnome.gnome-music
-    pkgs.gnome.gnome-weather
-    pkgs.gnome.gnome-maps
-    pkgs.gnome.gnome-characters
-    pkgs.gnome.gnome-logs
-    pkgs.gnome.tali # poker game
-    pkgs.gnome.iagno # go game
-    pkgs.gnome.hitori # sudoku game
-    pkgs.gnome.atomix # puzzle game
+    pkgs.gnome-contacts
+    pkgs.gnome-clocks
+    pkgs.gnome-music
+    pkgs.gnome-weather
+    pkgs.gnome-maps
+    pkgs.gnome-characters
+    pkgs.gnome-logs
     pkgs.gnome-calendar
     pkgs.gnome-photos
     pkgs.gnome-tour
@@ -33,6 +29,10 @@
     pkgs.evince # document viewer
     pkgs.totem # video player
     pkgs.yelp # help viewer
+    pkgs.tali # poker game
+    pkgs.iagno # go game
+    pkgs.hitori # sudoku game
+    pkgs.atomix # puzzle game
   ];
 
   services.xserver.excludePackages = [ pkgs.xterm ];
