6ff6e1ae23a5a76a62553c9e7eed973a4bdf427e
archiejudd <archie.judd@yahoo.com>
2024-08-27 20:42:13 +0100

    home-manager -> stable

--- flake.nix
+++ flake.nix
@@ -4,9 +4,8 @@
   inputs = {
 
     nixpkgs.url = "github:nixos/nixpkgs/nixos-24.05";
-    nixpkgs-unstable.url = "github:nixos/nixpkgs/nixos-unstable";
 
-    home-manager.url = "github:nix-community/home-manager";
+    home-manager.url = "github:nix-community/home-manager/release-24.05";
     home-manager.inputs.nixpkgs.follows = "nixpkgs";
 
     nix-darwin.url = "github:LnL7/nix-darwin";
