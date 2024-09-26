83e33186d18bdae4207dff58d03510a7141dbc77
archiejudd <archie.judd@yahoo.com>
2024-09-26 15:28:28 +0100

    add google-chrome to brew casks

--- system/modules/homebrew.nix
+++ system/modules/homebrew.nix
@@ -4,8 +4,15 @@
   # Install GUI programs and AppStore apps with homebrew
   homebrew = {
     enable = true;
-    casks =
-      [ "1password" "karabiner-elements" "vlc" "rectangle" "spotify" "maccy" ];
+    casks = [
+      "1password"
+      "karabiner-elements"
+      "vlc"
+      "rectangle"
+      "spotify"
+      "maccy"
+      "google-chrome"
+    ];
     masApps = {
       "1Password for Safari" = 1569813296;
       "istat Menus" = 1319778037;
