500eae42cf5bca40e7a9d937533b3cff1cc2c721
archiejudd <archie.judd@yahoo.com>
2025-02-10 18:17:32 +0000

    rpi home manager profile

--- flake.nix
+++ flake.nix
@@ -107,11 +107,23 @@
       };
 
       # Home manager only configurations
-      homeConfigurations = rec {
-        system = "x86_64-linux";
-        archiejudd = home-manager.lib.homeManagerConfiguration {
+      homeConfigurations = {
+        archiejudd = let system = "x86_64-linux";
+        in home-manager.lib.homeManagerConfiguration {
           pkgs = import nixpkgs { inherit system; };
           modules = [ ./home/users/work-nuc.nix ];
+          extraSpecialArgs = {
+            pkgs-unstable = import nixpkgs-unstable { inherit system; };
+            pkgs-fzf = import nixpkgs-fzf { system = system; };
+            nixpkgs = nixpkgs;
+            neovim-config = neovim-config;
+          };
+        };
+        # Rpi
+        archie = let system = "aarch64-linux";
+        in home-manager.lib.homeManagerConfiguration {
+          pkgs = import nixpkgs { inherit system; };
+          modules = [ ./home/users/personal-rpi.nix ];
           extraSpecialArgs = {
             pkgs-unstable = import nixpkgs-unstable { inherit system; };
             pkgs-fzf = import nixpkgs-fzf { system = system; };
