43c77e52683f97d2e12e4c01c6ef7853a48e3f1b
archiejudd <archie.judd@yahoo.com>
2025-12-15 18:18:15 +0000

    gromit-mpx -> draw-on-gnome

--- home/modules/gromit-mpx/default.nix
+++
@@ -1,8 +0,0 @@
-{ pkgs, ... }: {
-  home.packages = [ pkgs.gromit-mpx ];
-  home.file = {
-    ".config/autostart/gromit-mpx.desktop" = { source = ./gromit-mpx.desktop; };
-    ".config/gromit-mpx.cfg" = { source = ./gromit-mpx.cfg; };
-    ".config/gromit-mpx.ini" = { source = ./gromit-mpx.ini; };
-  };
-}
