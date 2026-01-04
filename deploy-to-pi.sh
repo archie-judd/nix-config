62fb5f488c370efdd664ce47951a88e0e8b2d6a3
archiejudd <archie.judd@yahoo.com>
2026-01-04 18:25:49 +0000

    rm deploy-to-pi

--- deploy-to-pi.sh
+++
@@ -1,9 +0,0 @@
-#!/usr/bin/env bash
-set -euo pipefail
-
-USER="archie"
-PI_HOST="${USER}@raspberry-pi.local"
-
-nix build ".#homeConfigurations.${USER}.activationPackage"
-nix copy --to "ssh://${PI_HOST}" ./result
-ssh "$PI_HOST" "$(readlink ./result)/activate"
