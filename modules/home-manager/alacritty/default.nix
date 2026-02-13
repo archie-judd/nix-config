c15670c84d4f2d9b9051b5816c7704a75edb4c2d
archiejudd <archie.judd@yahoo.com>
2026-02-13 23:18:17 +0000

    chore(mac): change alt

--- modules/home-manager/alacritty/default.nix
+++ modules/home-manager/alacritty/default.nix
@@ -44,7 +44,7 @@
             style = "BoldItalic";
           };
         };
-        window = { option_as_alt = "OnlyRight"; };
+        window = { option_as_alt = "OnlyLeft"; };
       };
   };
 }
