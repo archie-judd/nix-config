67b33a1dee263c2cdc8fc5c58e3d8f20e1cd710f
archiejudd <archiejudd@proton.me>
2026-04-11 15:15:18 +0100

    chore: update tasks + notes

--- modules/home-manager/notes.nix
+++ modules/home-manager/notes.nix
@@ -2,12 +2,12 @@
 
 {
   home.sessionVariables = {
-    NOTES_PATH =
-      "${config.home.homeDirectory}/workspaces/personal/notes/notes.md";
+    NOTES_PATH = "${config.home.homeDirectory}/workspaces/personal/notes.md";
   };
   programs.bash.initExtra = ''
     n() {
       local file="$NOTES_PATH"
+      [[ -f "$file" ]] || { mkdir -p "$(dirname "$file")" && touch "$file"; }
       case "''${1:-}" in
         "") $EDITOR "$file" ;;
         *)  printf "\n## %s\n\n%s\n" "$(date '+%F %T')" "$*" >> "$file" ;;
--- modules/home-manager/tasks.nix
+++ modules/home-manager/tasks.nix
@@ -2,12 +2,12 @@
 
 {
   home.sessionVariables = {
-    TASKS_PATH =
-      "${config.home.homeDirectory}/workspaces/personal/notes/tasks.md";
+    TASKS_PATH = "${config.home.homeDirectory}/workspaces/personal/tasks.md";
   };
   programs.bash.initExtra = ''
     t() {
       local file="$TASKS_PATH"
+      [[ -f "$file" ]] || { mkdir -p "$(dirname "$file")" && touch "$file"; }
       case "''${1:-}" in
         "") $EDITOR "$file" ;;
         -l) grep -n "^- \[ \]" "$file" ;;
